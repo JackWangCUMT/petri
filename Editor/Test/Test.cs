ce96bc23958c48a2c3629d106283fdfd84b13ff6
Rémi Saurel <r4.saurel@gmail.com>
2015-12-24 14:37:41 +0100

    All the tests are now succeeding.

--- Editor/Test/Test.cs
+++ Editor/Test/Test.cs
@@ -31,12 +31,6 @@ namespace TestPetri
         }
 
         [Test()]
-        public void TestThatFails()
-        {
-            Assert.AreEqual("expected", "actual");
-        }
-
-        [Test()]
         public void TestThatSucceeds()
         {
             Assert.AreEqual("expected", "expected");
@@ -45,19 +39,9 @@ namespace TestPetri
         [Test()]
         public void TestThatThrows1()
         {
-            Assert.Throws(typeof(Exception), () => { throw new Exception("message");} );
-        }
-
-        [Test()]
-        public void TestThatThrows2()
-        {
-            throw new Exception("message");
-        }
-
-        [Test()]
-        public void TestThatDoesntCompile()
-        {
-            arg!
+            Assert.Throws(typeof(Exception), () => {
+                throw new Exception("message");
+            });
         }
     }
 }
