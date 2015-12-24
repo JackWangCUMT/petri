094a5f2f0e7f53ec7e2a6dfea191f07ff278a36a
Rémi Saurel <r4.saurel@gmail.com>
2015-12-25 00:21:04 +0100

    Added a clean script.

--- Editor/build.sh
+++ Editor/build.sh
@@ -2,5 +2,16 @@
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ ! -f "$dir"/clean.sh ]; then
+    echo '#!/bin/bash
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+mdtool build -t:Clean "$dir"/Petri.sln
+' > "$dir"/clean.sh
+
+    chmod +x "$dir"/clean.sh
+fi
+
 mdtool build "$dir"/Petri.sln
 
