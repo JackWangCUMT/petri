44fde5b2be5802ff65f83c6cfb055b9c6c1fdeee
Rémi Saurel <r4.saurel@gmail.com>
2015-12-24 13:44:03 +0100

    Added NUnit tests

---
+++ Editor/Test/Test.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestPetri
+{
+    [TestFixture()]
+    public class Test1
+    {
+        [SetUp()]
+        public void SetUp()
+        {
+            Console.WriteLine("SetUp");
+        }
+
+        [TearDown()]
+        public void TearDown()
+        {
+            Console.WriteLine("TearDown");
+        }
+
+        [TestFixtureSetUp()]
+        public void FixtureSetUp()
+        {
+        }
+
+        [TestFixtureTearDown()]
+        public void FixtureTearDown()
+        {
+        }
+
+        [Test()]
+        public void TestThatFails()
+        {
+            Assert.AreEqual("expected", "actual");
+        }
+
+        [Test()]
+        public void TestThatSucceeds()
+        {
+            Assert.AreEqual("expected", "expected");
+        }
+
+        [Test()]
+        [ExpectedException(typeof(Exception))]
+        public void TestThatThrows1()
+        {
+            throw new Exception("message");
+        }
+
+        [Test()]
+        public void TestThatThrows2()
+        {
+            throw new Exception("message");
+        }
+    }
+}
+
---
+++ Editor/Test/Test2.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestPetri
+{
+    [TestFixture()]
+    public class Test2
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
+
