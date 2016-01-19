b55d6c83979ac0508a0a8c224ff6c13ef0efee7d
Rémi Saurel <r4.saurel@gmail.com>
2016-01-19 22:15:22 +0100

    Added a common base class for the generated petri net libraries.

---
+++ Runtime/CSharp/GeneratedDynamicLib.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Petri.Runtime
+{
+    /// <summary>
+    /// The base class of a generated petri net dynamic library
+    /// </summary>
+    public abstract class GeneratedDynamicLib
+    {
+        /// <summary>
+        /// Accesses the DynamicLib instance embedded into the library
+        /// </summary>
+        /// <value>The lib.</value>
+        public DynamicLib Lib {
+            get;
+            protected set;
+        }
+    }
+}
+
