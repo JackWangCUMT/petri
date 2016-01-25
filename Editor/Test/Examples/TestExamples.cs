b2165319095c5bfea11d6e9cb364884d0379b5ae
Rémi Saurel <r4.saurel@gmail.com>
2016-01-25 22:01:19 +0100

    Added a simple C example and a test that compiles it.

--- Editor/Test/Examples/TestExamples.cs
+++ Editor/Test/Examples/TestExamples.cs
@@ -61,6 +61,22 @@ namespace Petri.Test
             Assert.AreEqual("", stdout);
             Assert.AreEqual("", stderr);
         }
+
+        [Test()]
+        public void TestCExample()
+        {
+            // GIVEN launch arguments requesting code generation and compilation of the C#.petri example
+            string[] args = { "-g", "-c", "../../../Examples/C.petri" };
+            string stdout, stderr;
+
+            // WHEN the invocation is made
+            int result = Utility.InvokeCompiler(args, out stdout, out stderr);
+
+            // THEN no error is returned.
+            Assert.AreEqual(0, result);
+            Assert.AreEqual("", stdout);
+            Assert.AreEqual("", stderr);
+        }
     }
 }
 
---
+++ Examples/TestC.h
@@ -0,0 +1,19 @@
+#include "Runtime/C/Petri.h"
+#include <stdio.h>
+
+typedef enum ActionResult ActionResult;
+
+static ActionResult action1() {
+	printf("Action1!\n");
+	return 0;
+}
+
+static ActionResult action2() {
+        printf("Action2!\n");
+        return 0;
+}
+
+static bool condition1(Petri_actionResult_t result) {
+        printf("Condition1!\n");
+        return true;
+}
