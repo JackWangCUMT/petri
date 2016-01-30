cf1c0a280cd52e2aee05d591f316b20ff7d0fc7b
Rémi Saurel <r4.saurel@gmail.com>
2016-01-30 22:54:55 +0100

    Added a test for variables in C++ petri net.

---
+++ Editor/Test/Examples/Variables/TestVariables.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Petri.Test
+{
+    [TestFixture()]
+    public class TestVariables
+    {
+        [Test()]
+        public void TestCppExampleCompilation()
+        {
+            // GIVEN launch arguments requesting code generation and compilation of the Cpp.petri example
+            string[] args = { "-kcg", "../../../Examples/CppVar.petri" };
+            string stdout, stderr;
+
+            // WHEN the invocation is made
+            int result = Utility.InvokeCompiler(args, out stdout, out stderr);
+
+            // THEN no error is returned.
+            Assert.AreEqual("", stderr);
+            Assert.AreEqual("", stdout);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test()]
+        public void TestCppExampleExecution()
+        {
+            // GIVEN launch arguments requesting code generation and compilation of the Cpp.petri example
+            string[] args = { "-r", "../../../Examples/CppVar.petri" };
+            string stdout, stderr;
+
+            // WHEN the invocation is made
+            int result = Utility.InvokeCompiler(args, out stdout, out stderr);
+
+            // THEN no error is returned.
+            Assert.AreEqual("", stderr);
+            Assert.AreEqual("", stdout);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
+
